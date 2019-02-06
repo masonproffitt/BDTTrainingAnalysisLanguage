8219dafb708bce146409eff68115478fa3b8795a
Gordon Watts <gwatts@uw.edu>
2019-02-06 01:03:59 -0800

    Simple Outline of What Code We Want

---
+++ clientlib/DataSets.py
@@ -0,0 +1,5 @@
+# Code to help specify and work with datasets on the client end.
+
+class EventDataSet:
+    def __init__ (self, args):
+        self._url = args[0]
---
+++ train.py
@@ -0,0 +1,18 @@
+# Run the training
+
+# Get the import working from local files
+from clientlib.DataSets import EventDataSet
+
+# The input file we are going to use to do the training
+f = EventDataSet(r"file://G:\mc16_13TeV\AOD.16300985._000011.pool.root.1")
+
+# Turn into an event stream that is known by ATLAS.
+events = f.AsATLASEvents()
+
+# Next, get the jet pT's, which we want to look at.
+jet_pts = events.SelectMany(lambda e: e.Jets).Calibrate().Select(lambda j: j.pT)
+
+# Save it to a dataframe
+training_df = jet_pts.AsPandasDF()
+
+print (training_df)

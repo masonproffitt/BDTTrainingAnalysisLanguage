25a2136de200b5258dcd0762e9204cbabf7afa4d
Gordon Watts <gwatts@uw.edu>
2019-02-09 18:10:10 -0800

    Print out the contents of a jet
    Including its pT.

--- R21Code/query.cxx
+++ R21Code/query.cxx
@@ -1,7 +1,7 @@
 #include <AsgTools/MessageCheck.h>
 #include <analysis/query.h>
-
-
+#include <xAODEventInfo/EventInfo.h>
+#include <xAODJet/JetContainer.h>
 
 query :: query (const std::string& name,
                                   ISvcLocator *pSvcLocator)
@@ -14,8 +14,6 @@ query :: query (const std::string& name,
   // rather go into the initialize() function.
 }
 
-
-
 StatusCode query :: initialize ()
 {
   // Here you do everything that needs to be done at the very
@@ -25,14 +23,27 @@ StatusCode query :: initialize ()
   return StatusCode::SUCCESS;
 }
 
-
-
 StatusCode query :: execute ()
 {
   // Here you do everything that needs to be done on every single
   // events, e.g. read input variables, apply cuts, and fill
   // histograms and trees.  This is where most of your actual analysis
   // code will go.
+
+  // retrieve the eventInfo object from the event store
+  const xAOD::EventInfo *eventInfo = nullptr;
+  ANA_CHECK (evtStore()->retrieve (eventInfo, "EventInfo"));
+  ANA_MSG_INFO ("in execute, runNumber = " << eventInfo->runNumber() << ", eventNumber = " << eventInfo->eventNumber());
+
+  // Loop over the jets, and print out some nice info.
+  const xAOD::JetContainer* jets = 0;
+  ANA_CHECK (evtStore()->retrieve( jets, "AntiKt4EMTopoJets"));
+  ANA_MSG_INFO ("execute(): number of jets = " << jets->size());
+
+  for (auto jet : *jets) {
+    ANA_MSG_INFO ("execute(): jet pt = " << (jet->pt() * 0.001) << " GeV"); // just to print out something
+  }
+
   return StatusCode::SUCCESS;
 }
 
